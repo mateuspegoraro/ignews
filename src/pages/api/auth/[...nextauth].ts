e19430deb26b0120397723c407cc64ffefd6c9a7
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-05-07 12:22:04 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+import {query as q} from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import {fauna} from '../../../services/fauna'
+ 
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -8,4 +11,21 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(q.Collection('users'),
+            {
+              data: { email }
+            }
+          )
+        )
+        return true
+      } catch (error) {
+        return false        
+      }
+    },
+  }
 })
