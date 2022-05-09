29ec68c60510446b757ecee774775bdb8c9f1b9c
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-05-09 07:53:33 -0300

    Gerando sessão de checkout

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,10 +1,24 @@
+import { signIn, useSession } from 'next-auth/react';
+
 import styles from './styles.module.scss'
+import {stripe} from '../../services/stripe'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+
+    const session = useSession();
+
+    function handleSubscribe() {
+        if(!session.data) {
+            signIn('github');
+            return;
+        }
+
+        // fazer requisição para a rota de subscribe
+    }
 
     return (
         <button
