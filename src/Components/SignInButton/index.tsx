25cf4d02cc8c937360a04e849ab6d8a959a7930e
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-05-12 08:32:40 -0300

    Webhooks do Stripe

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -6,7 +6,6 @@ import {signIn, signOut, useSession} from 'next-auth/react'
 export function SignInButton() {
     const { data: session } = useSession()
 
-    console.log(session)
 
     return session ? 
         (
