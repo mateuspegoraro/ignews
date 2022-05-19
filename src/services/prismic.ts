aa0ba41ad1a482d8800a9ad4fd118c4c1a10ef93
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-05-19 08:06:17 -0300

    Consumindo API do Prismic

---
+++ src/services/prismic.ts
@@ -0,0 +1,10 @@
+import Prismic from '@prismicio/client'
+
+export const getPrismicClient = (req?: unknown) => {
+    const prismic = Prismic.client(process.env.PRISMIC_REPO_URL, {
+      req,
+      accessToken: process.env.PRISMIC_ACCESS_TOKEN
+    })
+    return prismic;
+} 
+    
