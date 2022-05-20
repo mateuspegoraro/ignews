85628b5ce7c2d2cffe18154a2357184f6721c2d1
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-05-20 08:04:22 -0300

    prettier

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -15,66 +15,69 @@ async function buffer(readable: Readable) {
   return Buffer.concat(chunks);
 }
 
-
 // Isso por que o next tem um entendimento padrão do que vem da requisição, e acaba tentando fazer o parser dela.
 // No caso do stream precisamos desabilitar isso
 // https://nextjs.org/docs/api-routes/api-middlewares
 export const config = {
   api: {
-    bodyParser: false
-  }
-}
+    bodyParser: false,
+  },
+};
 
 const relevantEvents = new Set([
-  'checkout.session.completed',
-  'customer.subscription.updated',
-  'customer.subscription.deleted',
-])
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if(req.method === 'POST') {
-    const buf = await buffer(req)
+  if (req.method === "POST") {
+    const buf = await buffer(req);
 
-    const secret = req.headers['stripe-signature']
+    const secret = req.headers["stripe-signature"];
 
     let event: Stripe.Event;
 
     try {
-      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
     } catch (error) {
-      return res.status(400).send(`Webhook error: ${error.message}`)
+      return res.status(400).send(`Webhook error: ${error.message}`);
     }
 
+    const { type } = event;
 
-    const {type} = event;
-
-    if(relevantEvents.has(type)) {
+    if (relevantEvents.has(type)) {
       try {
-        switch(type){
-          case 'customer.subscription.updated':
-          case 'customer.subscription.deleted':
+        switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
             const subscription = event.data.object as Stripe.Subscription;
 
             await saveSubscription(
-                subscription.id, 
-                subscription.customer.toString(),
-                false
-              );
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
 
             break;
-          case 'checkout.session.completed':
-            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
             await saveSubscription(
-                checkoutSession.subscription.toString(), 
-                checkoutSession.customer.toString(),
-                true
-              )
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
             break;
           default:
-            throw new Error('Unhandled event');  
+            throw new Error("Unhandled event");
         }
       } catch (error) {
-        return res.json({error: 'Webhook hanfled failed.'})
+        return res.json({ error: "Webhook hanfled failed." });
       }
       // console.log('Evento recebido: ', event)
     }
